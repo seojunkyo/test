12562d027b6596478c03944b200dda92a644ef10
seojunkyo <seojk2me@gmail.com>
2025-11-11 16:20:02 +0900

    Update AIAssistantUI.jsx

--- components/AIAssistantUI.jsx
+++ components/AIAssistantUI.jsx
@@ -80,7 +80,7 @@ export default function AIAssistantUI() {
 
   const onLogin = () => {
     localStorage.setItem("isLoggedIn", true);
-    window.location.href= = `${apiUrl}/sso`
+    window.location.href = `${apiUrl}/sso`
   };
 
   useEffect(() => {
